104d26b326d0c92cb3d69043c32faa5edb68a5af
takashabe <takashabe.public@gmail.com>
2017-03-26 19:11:14 +0900

    add minimum Memory datastore

--- datastore.go
+++ datastore.go
@@ -3,6 +3,14 @@ package queue
 import (
 	"fmt"
 	"io/ioutil"
+	"sync"
+
+	"github.com/pkg/errors"
+)
+
+// Datastore errors
+var (
+	ErrNotFoundMessage = errors.New("not found entry")
 )
 
 type Datastore interface {
@@ -21,4 +29,38 @@ func LoadDatastore(path string) (Datastore, error) {
 	fmt.Println(string(data))
 
 	return nil, nil
+}
+
+// Datastore driver at "in memory"
+type Memory struct {
+	messages map[string]Message
+	mu       sync.Mutex
+}
+
+// Create memory object
+func NewMemory() *Memory {
+	return &Memory{
+		messages: make(map[string]Message),
+	}
+}
+
+// Save message to memory
+func (m *Memory) Set(item Message) error {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	m.messages[item.ID] = item
+	return nil
+}
+
+// Get message from memory
+func (m *Memory) Get(id string) (Message, error) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	item, ok := m.messages[id]
+	if !ok {
+		return Message{}, errors.Wrapf(ErrNotFoundMessage, fmt.Sprintf("id=%s", id))
+	}
+	return item, nil
 }
