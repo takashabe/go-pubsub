f563956c11d3156ebd6149af6842906dec6b9d05
takashabe <takashabe.public@gmail.com>
2017-03-29 00:33:27 +0900

    add delete test

--- datastore_test.go
+++ datastore_test.go
@@ -68,3 +68,38 @@ func TestGet(t *testing.T) {
 		}
 	}
 }
+
+func TestDeleteStore(t *testing.T) {
+	msgA := Message{ID: "a"}
+	msgB := Message{ID: "b"}
+	baseStore := Memory{
+		store: map[interface{}]interface{}{"a": msgA, "b": msgB},
+	}
+
+	cases := []struct {
+		input       string
+		expectStore map[interface{}]interface{}
+		expectErr   error
+	}{
+		{
+			"a",
+			map[interface{}]interface{}{"b": msgB},
+			nil,
+		},
+		{
+			// delete to non exist key
+			"a",
+			map[interface{}]interface{}{"b": msgB},
+			nil,
+		},
+	}
+	for i, c := range cases {
+		err := baseStore.Delete(c.input)
+		if errors.Cause(err) != c.expectErr {
+			t.Errorf("#%d: want %v, got %v", i, c.expectErr, err)
+		}
+		if !reflect.DeepEqual(baseStore.store, c.expectStore) {
+			t.Errorf("#%d: want %v, got %v", i, c.expectStore, baseStore.store)
+		}
+	}
+}
