df2c99ea0bb2c8a6fc639c480a9c35ce6de3618a
takashabe <takashabe.public@gmail.com>
2017-04-10 09:38:19 +0900

    add delete test

--- server_test.go
+++ server_test.go
@@ -17,7 +17,8 @@ func dummyClient(t *testing.T) *http.Client {
 	}
 }
 
-func setupServer(t *testing.T) {
+func setupServer(t *testing.T) *httptest.Server {
+	// setup datastore
 	s, err := NewServer("testdata/config.yaml")
 	if err != nil {
 		t.Fatalf("failed to NewServer, err=%v", err)
@@ -25,11 +26,13 @@ func setupServer(t *testing.T) {
 	if err := s.InitDatastore(); err != nil {
 		t.Fatalf("failed to InitDatastore, err=%v", err)
 	}
+
+	// setup http server
+	return httptest.NewServer(routes())
 }
 
 func TestCreateAndGetTopic(t *testing.T) {
-	setupServer(t)
-	ts := httptest.NewServer(routes())
+	ts := setupServer(t)
 	defer ts.Close()
 
 	cases := []struct {
@@ -86,6 +89,53 @@ func TestCreateAndGetTopic(t *testing.T) {
 		}
 		if got, _ := ioutil.ReadAll(res.Body); !reflect.DeepEqual(got, c.expectGetBody) {
 			t.Errorf("#%d: want %s, got %s", i, c.expectGetBody, got)
+		}
+	}
+}
+
+func TestDelete(t *testing.T) {
+	ts := setupServer(t)
+	defer ts.Close()
+
+	// create base topics
+	client := dummyClient(t)
+	puts := []string{"a", "b"}
+	for i, p := range puts {
+		req, err := http.NewRequest("PUT", ts.URL+"/topic/create/"+p, nil)
+		if err != nil {
+			t.Fatalf("#%d: failed to create request", i)
+		}
+		res, err := client.Do(req)
+		if err != nil {
+			t.Fatalf("#%d: failed to send request", i)
+		}
+		defer res.Body.Close()
+	}
+
+	cases := []struct {
+		input      string
+		expectCode int
+		expectBody []byte
+	}{
+		{"a", http.StatusNoContent, []byte("")},
+		{"a", http.StatusNotFound, []byte("{\"reason\":\"topic already not exist\"}")},
+	}
+	for i, c := range cases {
+		req, err := http.NewRequest("DELETE", ts.URL+"/topic/delete/"+c.input, nil)
+		if err != nil {
+			t.Fatalf("#%d: failed to create request", i)
+		}
+		res, err := client.Do(req)
+		if err != nil {
+			t.Fatalf("#%d: failed to send request", i)
+		}
+		defer res.Body.Close()
+
+		if got := res.StatusCode; got != c.expectCode {
+			t.Errorf("#%d: want %d, got %d", i, c.expectCode, got)
+		}
+		if got, _ := ioutil.ReadAll(res.Body); !reflect.DeepEqual(got, c.expectBody) {
+			t.Errorf("#%d: want %s, got %s", i, c.expectBody, got)
 		}
 	}
 }
