57b0893d6e91810e8b701cbac0589588dd6fe3e1
takashabe <takashabe.public@gmail.com>
2017-04-20 23:44:21 +0900

    add collect subscription method

--- models/datastore_subscription.go
+++ models/datastore_subscription.go
@@ -43,6 +43,26 @@ func (ts *DatastoreSubscription) Get(key string) (*Subscription, error) {
 	return v, nil
 }
 
+func (ts *DatastoreSubscription) CollectByTopicID(topicID string) ([]*Subscription, error) {
+	switch ts.store.(type) {
+	case *Memory, *Redis, *MySQL:
+		res := make([]*Subscription, 0)
+		for _, v := range ts.store.Dump() {
+			s, ok := v.(*Subscription)
+			if !ok {
+				return nil, errors.Wrapf(ErrNotMatchTypeSubscription, "key=%s", topicID)
+			}
+			if s.Topic.Name != topicID {
+				continue
+			}
+			res = append(res, s)
+		}
+		return res, nil
+	default:
+		return nil, ErrNotSupportOperation
+	}
+}
+
 func (ts *DatastoreSubscription) List() ([]*Subscription, error) {
 	values := ts.store.Dump()
 	res := make([]*Subscription, 0, len(values))
