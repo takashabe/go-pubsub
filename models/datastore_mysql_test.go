a7ca75fbd490e37010eb20245d56baedcc9a94ac
takashabe <takashabe.public@gmail.com>
2017-05-10 23:44:15 +0900

    Add mysql datastore adapter methods and test

--- models/datastore_mysql_test.go
+++ models/datastore_mysql_test.go
@@ -1,9 +1,11 @@
 package models
 
 import (
+	"database/sql"
 	"reflect"
 	"testing"
 
+	"github.com/pkg/errors"
 	"github.com/takashabe/go-fixture"
 	_ "github.com/takashabe/go-fixture/mysql"
 )
@@ -20,13 +22,14 @@ func dummyMySQL(t *testing.T) *MySQL {
 	if err != nil {
 		t.Fatalf("failed to connect mysql, got err %v", err)
 	}
+	return c
+}
 
-	f := fixture.NewFixture(c.conn, "mysql")
+func clearTable(t *testing.T, db *sql.DB) {
+	f := fixture.NewFixture(db, "mysql")
 	if err := f.LoadSQL("testdata/empty_table.sql"); err != nil {
 		t.Fatalf("failed to execute fixture, got err %v", err)
 	}
-
-	return c
 }
 
 func TestMySQLSetAndGet(t *testing.T) {
@@ -46,6 +49,7 @@ func TestMySQLSetAndGet(t *testing.T) {
 			t.Fatalf("#%d: failed to encode data, got err %v", i, err)
 		}
 		client := dummyMySQL(t)
+		clearTable(t, client.conn)
 		if err := client.Set(c.key, encode); err != nil {
 			t.Fatalf("#%d: failed to set, key=%v, value=%v, got err %v", i, c.key, c.value, err)
 		}
@@ -65,6 +69,98 @@ func TestMySQLSetAndGet(t *testing.T) {
 		}
 		if !reflect.DeepEqual(c.value, m) {
 			t.Errorf("#%d: get value want %v, got %v", i, c.value, m)
+		}
+	}
+}
+
+func TestMySQLDelete(t *testing.T) {
+	cases := []struct {
+		key       interface{}
+		value     interface{}
+		expectErr error
+	}{
+		{
+			"a",
+			&Message{ID: "a"},
+			sql.ErrNoRows,
+		},
+	}
+	for i, c := range cases {
+		// set
+		encode, err := EncodeGob(c.value)
+		if err != nil {
+			t.Fatalf("#%d: failed to encode data, got err %v", i, err)
+		}
+		client := dummyMySQL(t)
+		clearTable(t, client.conn)
+		if err := client.Set(c.key, encode); err != nil {
+			t.Fatalf("#%d: failed to set, key=%v, value=%v, got err %v", i, c.key, c.value, err)
+		}
+
+		// delete
+		if err := client.Delete(c.key); err != nil {
+			t.Fatalf("#%d: failed to delete entry, got err %v", i, err)
+		}
+
+		// get
+		_, err = client.Get(c.key)
+		if errors.Cause(err) != c.expectErr {
+			t.Errorf("#%d: ", i, c.key, err)
+		}
+	}
+}
+
+func TestMySQLDump(t *testing.T) {
+	type kv struct {
+		id    interface{}
+		value interface{}
+	}
+	cases := []struct {
+		inputEntries []kv
+		expect       map[interface{}]interface{}
+	}{
+		{
+			[]kv{
+				{id: "a", value: &Message{ID: "a"}},
+				{id: "b", value: &Message{ID: "b"}},
+			},
+			map[interface{}]interface{}{
+				"a": &Message{ID: "a"},
+				"b": &Message{ID: "b"},
+			},
+		},
+	}
+	for i, c := range cases {
+		client := dummyMySQL(t)
+		clearTable(t, client.conn)
+
+		// set
+		for _, e := range c.inputEntries {
+			encode, err := EncodeGob(e.value)
+			if err != nil {
+				t.Fatalf("#%d: failed to encode data, got err %v", i, err)
+			}
+			if err := client.Set(e.id, encode); err != nil {
+				t.Fatalf("#%d: failed to set, key=%v, value=%v, got err %v", i, e.id, e.value, err)
+			}
+		}
+
+		// dump
+		dump, err := client.Dump()
+		if err != nil {
+			t.Fatalf("#%d: failed to dump, goo err %v", i, err)
+		}
+
+		replaces := make(map[interface{}]interface{}, len(c.inputEntries))
+		for k, v := range dump {
+			m, err := DecodeGobMessage(v.([]byte))
+			if err != nil {
+				t.Fatalf("#%d: failed to decode data, got err %v", i, err)
+			}
+			replaces[k] = m
+		}
+		if !reflect.DeepEqual(c.expect, replaces) {
+			t.Errorf("#%d: get value want %v, got %v", i, c.expect, replaces)
 		}
 	}
 }
