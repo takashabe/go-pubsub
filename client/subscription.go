38f097aa3f8ed262b172ac26d3eac4d7b0e124b3
takashabe <takashabe.public@gmail.com>
2017-08-06 21:18:09 +0900

    Add topic client functions

--- client/subscription.go
+++ client/subscription.go
@@ -1,3 +1,14 @@
 package client
 
-type Subscription struct{}
+// Subscription is a accessor to a server subscription
+type Subscription struct {
+	id string
+	s  service
+}
+
+func newSubscription(id string, s service) *Subscription {
+	return &Subscription{
+		id: id,
+		s:  s,
+	}
+}
--- client/topic.go
+++ client/topic.go
@@ -5,14 +5,30 @@ import "context"
 // Topic is a accessor to a server topic
 type Topic struct {
 	id string
+	s  service
 }
 
 // Exists return whether the topic exists on the server.
-func (t *Topic) Exists() (bool, error) {
 	return false, nil
+func (t *Topic) Exists(ctx context.Context) (bool, error) {
+	return t.s.topicExists(ctx, t.id)
+}
+
+// Delete deletes the topic
+func (t *Topic) Delete(ctx context.Context) error {
+	return t.s.deleteTopic(ctx, t.id)
 }
 
 // Subscriptions returns subscription list matched topic
-func (t *Topic) Subscriptions(ctx context.Context) ([]Subscription, error) {
-	return nil, nil
+func (t *Topic) Subscriptions(ctx context.Context) ([]*Subscription, error) {
+	subIDs, err := t.s.listTopicSubscriptions(ctx, t.id)
+	if err != nil {
+		return nil, err
+	}
+
+	subs := []*Subscription{}
+	for _, id := range subIDs {
+		subs = append(subs, newSubscription(id, t.s))
+	}
+	return subs, nil
 }
