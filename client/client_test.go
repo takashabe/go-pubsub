99baaa96031930aa1bc5a5025d4acf5a3a5a1786
takashabe <takashabe.public@gmail.com>
2017-08-24 20:50:15 +0900

    Add Publish test

--- client/client_test.go
+++ client/client_test.go
@@ -20,6 +20,19 @@ func setupServer(t *testing.T) *httptest.Server {
 	return httptest.NewServer(server.Routes())
 }
 
+func createDummyTopics(t *testing.T, ts *httptest.Server) {
+	ctx := context.Background()
+	client, err := NewClient(ctx, ts.URL)
+	if err != nil {
+		t.Fatalf("failed to NewClient, error=%v", err)
+	}
+	for _, id := range []string{"topic1", "topic2"} {
+		_, err := client.CreateTopic(ctx, id)
+		if err != nil {
+			t.Fatalf("failed to create new topic, error=%v", err)
+		}
+	}
+}
 func TestCreateTopic(t *testing.T) {
 	ts := setupServer(t)
 	defer ts.Close()
@@ -44,6 +57,56 @@ func TestCreateTopic(t *testing.T) {
 		}
 		if !reflect.DeepEqual(c.expect, topic) {
 			t.Errorf("#%d: want %v, got %v", i, c.expect, topic)
+		}
+	}
+}
+
+func TestPublish(t *testing.T) {
+	ts := setupServer(t)
+	defer ts.Close()
+	createDummyTopics(t, ts)
+	ctx := context.Background()
+	client, err := NewClient(ctx, ts.URL)
+	if err != nil {
+		t.Fatalf("want non-error, got %v", err)
+	}
+
+	cases := []struct {
+		inputs []*Message
+	}{
+		{
+			[]*Message{
+				&Message{Data: []byte(`msg1`)},
+				&Message{Data: []byte(`msg2`), Attributes: map[string]string{"msg2": "foo"}},
+			},
+		},
+		{
+			[]*Message{
+				&Message{Data: []byte(`msg3`)},
+				&Message{},
+			},
+		},
+	}
+	for i, c := range cases {
+		// asynchronously publish messages
+		topic := client.Topic("topic1")
+		idCh := make(chan string)
+		for mi, m := range c.inputs {
+			go func(ci, gi int, gm *Message) {
+				result := topic.Publish(ctx, gm)
+				msgID, err := result.Get(ctx)
+				if err != nil {
+					idCh <- ""
+					t.Fatalf("#%d-%d: failed to publish message, error=%v", ci, gi, err)
+				}
+				idCh <- msgID
+			}(i, mi, m)
+		}
+
+		for cntCh := 0; cntCh < len(c.inputs); cntCh++ {
+			if msgID := <-idCh; msgID == "" {
+				t.Errorf("#%d-%d: want non-empty message id", i, cntCh)
+			}
 		}
 	}
 }
